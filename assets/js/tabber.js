14aaf038b0a11bfb67e68322d139cf65e662e580
Tilkesh Joshi <70138331+Tilkesh@users.noreply.github.com>
2023-09-25 12:18:38 +0530

    Add files via upload

---
+++ assets/js/tabber.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+    $('.tab-content:first').show();
+    $('.tab-navigation li:first').addClass('active');
+
+    // $('.tab-navigation li').click(function(){
+    $('.tab-navigation li').on('click',function(){
+            let index =$(this).index();
+            $('.tab-navigation li').removeClass('active');
+            $(this).addClass('active');
+            $('.tab-content').hide();
+            $('.tab-content').eq(index).show();
+            $('.tab-content').addClass('tabber_newclass');
+    });
+});
+
+
+jQuery(".foodtech_slider").owlCarousel({
+    autoplay: false,
+    rewind: true, /* use rewind if you don't want loop */
+    margin: 0,
+    loop:true,
+    center: true,
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+      1199: {
+        items: 1
+      },
+  
+      1200: {
+        items: 3
+      }
+  
+    
+    }
+  });
+
+
+  jQuery(".foodtech_slider-single").owlCarousel({
+    autoplay: false,
+    items:1,
+    loop:true,
+    rewind: true, /* use rewind if you don't want loop */
+    margin: 0,
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    slideBy:1
+  });
